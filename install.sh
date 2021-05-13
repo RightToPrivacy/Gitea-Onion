4a6f5357b1d3b70ab53e250d44017ef823051120
RightToPrivacy <58191050+RightToPrivacy@users.noreply.github.com>
2021-05-13 01:54:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@
 # More updates coming to this later. File an issue/email if you notice problem. Thank you! :)
 #
 # Contact if problem w/shell script and I will fix: <righttoprivacy[at]tutanota.com> (NO AFFILIATION WITH GITEA)
+# Read my stuff (most posts are public): https://www.buymeacoffee.com/politictech/posts
 #
-# If you want to download the excellent Gitea Project, or read their documentation see: https://gitea.io/en-us/ 
-# (I have NO AFFILIATION) simply sharing something- you need proxy pass through to setup Hidden Service)
+# If you want to download the excellent Gitea Project (required for this), or read their documentation see: https://gitea.io/en-us/ 
+# (this has no affiliation) simply sharing method to make Gitea work as a hidden service (standard method w/not work)
 #
 
 # VARIABLES
@@ -29,6 +30,23 @@ cp default $nginxconf
 echo ' '
 echo ' '
 
+echo "Checking for iptables..."
+{
+if [ ! -f /usr/sbin/iptables ]; then
+    echo "iptables not found!"
+    exit 0
+fi
+}
+
+echo "iptables OK. Setting rules..."
+sleep 2
+
+sudo iptables -A INPUT -p tcp -s localhost --dport 3000 -j ACCEPT
+sudo iptables -A INPUT -p tcp -s --dport 3000 -j DROP
+
+echo 'To retain blocking (outside your onion address), install iptables-persistent package.'
+echo "To clear newly blocking rules either reboot or issue: sudo iptables --flush"
+
 if [[ -f $oname ]] then
     echo 'Your New Gitea Onion (Tor Hidden Service): '
     cat $oname
@@ -36,7 +54,7 @@ if [[ -f $oname ]] then
     sleep 1.5
     echo ' '
     echo 'Enjoy your Gitea encrypted onion server!'
-    echo 'More information will be found on my pages.'
+    echo 'More information will be found on README.md.'
 
 else
     echo "Onion hostname file does not exist. Check your setup."
